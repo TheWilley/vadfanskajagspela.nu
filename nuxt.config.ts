b2f41c59dc7921577b8d6a78d7ffa121c700db9d
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-04-22 01:35:32 +0200

    fix: second attempt at making website work on pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
     css: ["bootstrap/dist/css/bootstrap.min.css", "assets/css/main.sass"],
     app: {
-        baseURL: "/dist"
+        baseURL: "/vadfanskajagspela.nu"
     },
 })
