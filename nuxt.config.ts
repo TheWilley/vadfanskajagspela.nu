e00e7ed87fbf01fe23a5617d30aab88a400f16ec
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-04-22 01:37:19 +0200

    fix: forgot to add a '/' at the end of the url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
     css: ["bootstrap/dist/css/bootstrap.min.css", "assets/css/main.sass"],
     app: {
-        baseURL: "/vadfanskajagspela.nu"
+        baseURL: "/vadfanskajagspela.nu/"
     },
 })
